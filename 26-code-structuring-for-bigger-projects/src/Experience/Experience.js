6b6409f933ebd5f4dbb06abe4fda518f0cd966ad
Machine Boy <machineboy.create@gmail.com>
2022-11-24 11:55:41 -0800

    first commit befote renderer class

---
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -0,0 +1,57 @@
+import * as THREE from 'three'
+import Sizes from "./Utils/Sizes.js"
+import Time from "./Utils/Time.js"
+import Camera from "./Camera.js"
+
+let instance = null
+
+export default class Experience 
+{
+    constructor(canvas)
+    {
+        if(instance)
+        {
+
+            return instance
+        }
+
+        instance = this
+        
+
+        //Global access
+        window.experience = this
+
+        //Options 
+        this.canvas = canvas
+
+        //Setup
+        this.sizes = new Sizes()
+        this.time = new Time()
+        this.scene = new THREE.Scene()
+        this.camera = new Camera()
+
+
+        // Sizes resize event
+        this.sizes.on('resize',()=>
+        {
+            this.resize()
+        })
+
+        //Time tick event
+        this.time.on('tick', ()=>
+        {
+            this.update()
+        })
+    }
+
+
+    resize()
+    {
+        this.camera.resize()
+    }
+
+    update()
+    {
+        this.camera.update()
+    }
+}
